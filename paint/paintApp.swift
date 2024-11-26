d4fc5f413b24ef08dafc3504b0b325b387708bba
Brenda Barraza <brendabarraza99@gmail.com>
2024-11-26 18:17:01 -0300

    Initial Commit

---
+++ paint/paintApp.swift
@@ -0,0 +1,17 @@
+//
+//  paintApp.swift
+//  paint
+//
+//  Created by brenda on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct paintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
